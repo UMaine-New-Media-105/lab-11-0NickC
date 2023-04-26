846a788691f21f0faed6a8e19de6589a06213e41
0NickC <124187302+0NickC@users.noreply.github.com>
2023-04-26 12:49:39 -0400

    Create Lab11Ex2SpaceInvaders.js
    https://editor.p5js.org/Nick_C/sketches/dPISC6r71

---
+++ Lab11Ex2SpaceInvaders.js
@@ -0,0 +1,44 @@
+
+/*
+Recreating the game:
+
+//create a home screen giving instructions on how to play the game and how to start the game
+
+//create a function for the ship
+//create a function for projectile
+
+//create a class for the enemy
+make enemy sprites spawn randomly and fall down from the top of the canvas
+
+//create a health bar
+give three heart icons to symolize lives
+when the enemy hits the bottom of the canvas you lose a life
+when all the lives are lost everything on the canvas stops moving and the game ends
+
+//assign movement to arrow keys
+pressing LEFT and RIGHT keys will move the ship in the direction pressed
+
+//assign keyPressed function to SPACE key
+if SPACE key is pressed, projectile will launch from ship
+
+//add collision bewteen projectile and enemy
+if projectile collildes with enemy, enemy sprite will be deleted
+
+//add collision between enemy and bottom of the canvas
+if enemy collides with bottom of the canvas, life is lost
+
+
+------------------
+
+Gameplay Improvements:
+1. Have enemy move at random speeds
+2. Add powerups you can collect, thigs that might change how fast you can fire, how many projectiles you shoot at once, etc.
+3. Add score mechanic for when enemys are hit
+4. Add scoreboard for when the game ends
+5. Add a way to go back to the start screen
+
+Graphic Improvements:
+1. Add color to background to make it look more like space
+2. Add more detailed appearance to the ship and enemies
+
+*/
